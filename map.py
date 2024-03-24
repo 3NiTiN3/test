976eb588907f75c094a93976ef0a2f4b98c6a078
3NiTiN3 <161741906+3NiTiN3@users.noreply.github.com>
2024-03-24 01:32:13 +0000

    new2

--- map.py
+++ map.py
@@ -1,22 +1,15 @@
-import folium
+import webbrowser
 
-# Latitude and longitude coordinates
-locations = [
-    {"latitude": 18.43138300, "longitude": -64.6230},
-    {"latitude": 20.00000000, "longitude": 77.00000000}
-]
+def show_location_on_map(latitude, longitude):
+    # Format the URL with latitude and longitude
+    url = f"https://www.google.com/maps/search/?api=1&query={latitude},{longitude}"
+    
+    # Open the URL in a web browser
+    webbrowser.open(url)
 
-# Create a map centered at a specific location
-mymap = folium.Map(location=[0, 0], zoom_start=2)
+# Example latitude and longitude
+latitude = "18.43138300"
+longitude = "-64.6230"
 
-# Add markers for each location
-for loc in locations:
-    folium.Marker(
-        location=[loc["latitude"], loc["longitude"]],
-        popup=f'Latitude: {loc["latitude"]}, Longitude: {loc["longitude"]}'
-    ).add_to(mymap)
-
-# Save the map to an HTML file
-mymap.save("world_map.html")
-
-print("Map created successfully. Open 'world_map.html' to view.")
+# Show the location on a map
+show_location_on_map(latitude, longitude)
